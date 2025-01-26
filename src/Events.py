72195580ed3517259778db28b468ace74aca3d06
Catherine Harrison <cate.e.harrison@gmail.com>
2025-01-25 20:58:53 -0500

    Made events make sense

--- src/Events.py
+++ src/Events.py
@@ -1,114 +1,117 @@
 import pandas as pd
 import random
 
-"""
-    PurdueEcoQuest begins the execution of the program
-    stats = [health, happiness, money, academics, ecopoints]
-    will intialize stats to [95, 80, random, 50, 0]
-    
-"""
+from flask import jsonify
 
-class Game:
-    global stats
-    global events
-    def __init__(self):
-        # [health, happiness, money, academics, EcoPoints]
-        self.stats = [95, 80, 0, 50, 0]
-        # setting money as random
-        stats[2] = random.randint(20, 300)
+stats = []
+events = pd.read_csv("events.csv")
+global curr_event
+global week
+def start():
+    #[health, happiness, money, academics, ecopoints]
+    stats = [90, 80, 0, 50, 0]
+    #their money is randomized
+    stats[2] = random.randint(20, 50)
+    week = 1
 
-        self.events = pd.read_csv("events.csv")
+    #set move-in to curr_event
+    curr_event = events.iloc[0].tolist()
 
-        #TODO return move-in info
+    return jsonify(
+        {
+            "Name": curr_event[0],
+            "Description": curr_event[1],
+            "Choice1" : curr_event[2],
+            "Choice2" : curr_event[4],
+        }
+    )
 
+def choice_made(choice):
+    modifiers = None
+    if (choice == 1):
+        modifiers = curr_event[3].tolist()
+    else:
+        modifiers = curr_event[5].tolist()
 
-    # Will return string to send to javascript
-    def get_event(self):
-        #if health is too low
-        if (stats[0] < 15):
-            ## TODO: trigger sick
-        #if happiness is too low
-        elif (stats[1] < 30):
-            ## TODO: trigger breakdown
-        # if academics too low
-        elif (stats[3] < 20):
-            ## TODO: trigger academic probation warning
-        else:
-            ##TODO: pick random event
+    modifiers = modifiers.split(",")
 
-    def update_stats(self, event_num, choice):
-        event = events.
+    # now update stats
+    # health
+    stats[0] += int(modifiers[0])
+    if stats[0] > 100:
+        stats[0] = 100
+    elif stats[0] < 0:
+        stats[0] = 0
 
-#TODO: create start(), get_event(), get_stats(), update_stats() func
+    #happiness
+    stats[1] += int(modifiers[1])
+    if stats[1] > 100:
+        stats[1] = 100
+    elif stats[1] < 0:
+        stats[1] = 0
 
+    #money
+    stats[2] += int(modifiers[2])
+    if stats[2] < 0:
+        stats[2] = 0
 
+    # academics
+    stats[3] += int(modifiers[3])
+    if stats[3] > 100:
+        stats[3] = 100
+    elif stats[3] < 0:
+        stats[3] = 0
 
-def PurdueEcoQuest():
-    # [health, happiness, money, academics, EcoPoints]
-    stats = [95, 80, 0, 50, 0]
-    # setting money as random
-    stats[2] = random.randint(20, 300)
+    # ecopoints
+    stats[4] += int(modifiers[4])
 
-    events = pd.read_csv("events.csv")
+    return jsonify(
+        {
+            "Health": stats[0],
+            "Happiness": stats[1],
+            "Money" : stats[2],
+            "Academics" : stats[3],
+            "EcoPoints" : stats[4],
+        }
+    )
 
-    # move-in counts as week 0
-    # TODO: activate move-in event
-    # for sixteen weeks, one event per week
-    for week in range(1, 17):
-        #if health is too low
-        if (stats[0] < 15):
-            ## TODO: trigger sick
-        #if happiness is too low
-        elif (stats[1] < 30):
-            ## TODO: trigger breakdown
-        # if academics too low
-        elif (stats[3] < 20):
-            ## TODO: trigger academic probation warning
-        else:
-            ##TODO: pick random event
+def get_event():
+    #check if semester is over
+    if (week > 16):
+        return jsonify(
+            {
+                "Name": "Summary",
+                "Desciption" : summary(),
+            }
+        )
 
-    #after 16 weeks, do summary
-    summary(stats)
+    week += 1
 
-"""
-    update_stats will update stats with modifiers and return new list
-"""
-def update_stats(currstats, modifier):
-    #update health
-    currstats[0] += modifier[0]
-    if (currstats[0] < 0):
-        currstats[0] = 0
-    elif (currstats[0] > 100):
-        currstats[0] = 100
+    #if health is too low
+    if (stats[0] < 15):
+        curr_event = events.iloc[1].tolist()
+    #if happiness is too low
+    elif (stats[1] < 30):
+        curr_event = events.iloc[2].tolist()
+    # if academics too low
+    elif (stats[3] < 20):
+        curr_event = events.iloc[3].tolist()
+    else:
+        rand_event_index = random.randint(4, len(events))
+        curr_event = events.iloc[rand_event_index].tolist()
 
-    #update happiness
-    currstats[1] += modifier[1]
-    if (currstats[1] < 0):
-        currstats[1] = 0
-
-
-    # update money
-    # a check to make sure they have enough money should have already been put in place
-    currstats[2] -= modifier[2]
-
-    # update academics
-    currstats[3] += modifier[3]
-    if (currstats[3] < 0):
-        currstats[3] = 0
-
-    # update ecopoints
-    currstats[4] += modifier[4]
-
-    return currstats
-
-def summary(stats):
-
-
-
-
-PurdueEcoQuest()
-
-
-
+    return jsonify(
+        {
+            "Name": curr_event[0],
+            "Description": curr_event[1],
+            "Choice1" : curr_event[2],
+            "Choice2" : curr_event[4],
+        }
+    )
 
 
+def summary():
+    sum = ""
+
+
+    return sum
